686a4f95df08ae5357141c7ca58ea47858a22d13
carnae <samarbaskar02@gmail.com>
2024-02-26 08:58:14 +0530

    removed repeaton feature

--- Data cleaning/DataCleaner.py
+++ Data cleaning/DataCleaner.py
@@ -2,11 +2,9 @@ import re
 
 
 def conseq(line, RepeatOn):
-    if not RepeatOn:
-        pattern = r"(.)\1"
-        return bool(re.search(pattern, line))
-    else:
-        return False
+    pattern = r"(.)\1"
+    return bool(re.search(pattern, line))
+
 
 def spec(line):
     pattern = r"[!@#$%^&*()_+{}\[\]:;<>,.?/\\|'\"~`-]"
@@ -21,11 +19,9 @@ print("opened files")
 
 counter = 0
 
-RepeatOn = False #Change this to True if you want words with repeated characters to be allowed
-
 for line in file:
     counter += 1
-    if conseq(line, RepeatOn) == False and spec(line) == False and len(line) >= 3:
+    if conseq(line)and spec(line) == False and len(line) >= 3:
         cleanfile.write(f"{(line.lower().strip())}\n")
     if counter % 1000 == 0:
         print(counter)
