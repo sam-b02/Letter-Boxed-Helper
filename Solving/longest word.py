8cc8717151266826dea757723dbd8ec487f3fceb
carnae <samarbaskar02@gmail.com>
2024-03-24 02:57:37 +0530

    performance improvements

--- Solving/longest word.py
+++ Solving/longest word.py
@@ -1,4 +1,8 @@
+import time
+
+
 def initboard():
+    combine = ""
     board = open("Letter Boxed\\Words\\letter boxed diagram.txt", "r")
     side = None
     top = set()
@@ -8,6 +12,8 @@ def initboard():
     for line in board:
         line = line.strip()
         if line:
+            if line not in {"top", "bottom", "left", "right"}:
+                combine += str(line)
             if line in ["top", "left", "right", "bottom"]:
                 side = line
             elif side:
@@ -20,8 +26,12 @@ def initboard():
                         right.add(char)
                     elif side == "bottom":
                         bottom.add(char)
-    combine = top.union(left, right, bottom)
-    return top, left, right, bottom, combine
+
+    alpha_vector = 0
+    for char in combine:
+        alpha_vector |= 1 << ord(char) - ord("a")
+
+    return top, left, right, bottom, alpha_vector
 
 
 def checkpos(x, top, left, right, bottom):
@@ -38,31 +48,38 @@ def checkpos(x, top, left, right, bottom):
     return temp
 
 
-def char_check(line, combine):
-    return all(char in combine for char in line)
+def precompute_bit_vector(word):
+    bit_vector = 0
+    for char in word:
+        bit_vector |= 1 << (ord(char) - ord("a"))
+    return bit_vector
 
 
-def word_check(line, combine):
-    if not char_check(line, combine):
-        return False
+def word_check(line):
     for i in range(1, len(line)):
         if line[i] in checkpos(line[i - 1], top, left, right, bottom):
             return False
     return True
 
 
+def get_words():
+    with open("Letter Boxed\Words\wordlist.txt", "r") as file:
+        return [line.strip() for line in file]
+
+
+start = time.time()
 Words = []
-top, left, right, bottom, combine = initboard()
-wordlist = open("Letter Boxed\\Words\\wordlist.txt", "r")
-counter = 0
+
+top, left, right, bottom, alpha_vector = initboard()
+
+wordlist = get_words()
+
+
 for line in wordlist:
     line = line.strip()
-    counter += 1
-    if counter % 50000 == 0:
-        print(f"Scanned {counter} lines so far")
-    valid = word_check(line, combine)
-    if valid:
-        Words.append(line)
+    if (precompute_bit_vector(line) | alpha_vector) == alpha_vector:
+        if word_check(line):
+            Words.append(line)
 
 print(f"Found {len(Words)} letterboxed words\n\nSorting the list...")
 Words.sort(key=len, reverse=True)
@@ -72,3 +89,7 @@ with open(r"Letter Boxed\Words\LongestWords.txt", "w") as file:
         file.write(line + "\n")
 
 print("Done! You can find the words at Letter Boxed\Words\LongestWords.txt")
+
+end = time.time()
+execution_time = end - start
+print(f"Execution time: {execution_time} seconds")
